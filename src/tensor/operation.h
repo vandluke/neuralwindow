34c4339eae27ce2e9b2054559f69581dbab74708
vandluke <lvand10011@gmail.com>
2023-07-10 05:59:23 -0400

    tensor operations in separate file

---
+++ src/tensor/operation.h
@@ -0,0 +1,29 @@
+
+#ifndef OPERATION_H
+#define OPERATION_H
+
+#include <datatype.h>
+#include <errors.h>
+
+struct tensor_t;
+enum operation_type_t;
+struct operation_t;
+
+#include <tensor.h>
+
+typedef enum operation_type_t
+{
+    OPERATION_ADDITION
+} operation_type_t;
+
+typedef struct operation_t
+{
+    struct tensor_t **operands;
+    uint32_t number_of_operands;
+    operation_type_t type;
+} operation_t;
+
+error_t *create_operation(operation_t **operation, runtime_t runtime);
+error_t *destroy_operation(operation_t *operation, runtime_t runtime);
+
+#endif
