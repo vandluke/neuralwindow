0680cea54c359c52d094983a665fb751eb582336
vandluke <lvand10011@gmail.com>
2023-08-20 06:39:18 -0400

    reduce_recover_dimensions

--- test/test_view.c
+++ test/test_view.c
@@ -135,21 +135,21 @@ END_TEST
 START_TEST(test_strides_from_shape)
 {
     uint32_t number_of_cases = 6;
-    uint32_t shapes[][MAX_RANK] = {
-        {2, 3, 4, 5},
-        {1, 10}, 
-        {2, 1, 1},
-        {10}, 
-        {10, 1, 2, 5},
-        {2, 2, 3},
+    uint32_t *shapes[] = {
+        (uint32_t[]) {2, 3, 4, 5},
+        (uint32_t[]) {1, 10}, 
+        (uint32_t[]) {2, 1, 1},
+        (uint32_t[]) {10}, 
+        (uint32_t[]) {10, 1, 2, 5},
+        (uint32_t[]) {2, 2, 3},
     };
-    uint32_t expected_strides[][MAX_RANK] = {
-        {60, 20, 5, 1},
-        {0, 1},
-        {1, 0, 0},
-        {1},
-        {10, 0, 5, 1},
-        {6, 3, 1},
+    uint32_t *expected_strides[] = {
+        (uint32_t[]) {60, 20, 5, 1},
+        (uint32_t[]) {0, 1},
+        (uint32_t[]) {1, 0, 0},
+        (uint32_t[]) {1},
+        (uint32_t[]) {10, 0, 5, 1},
+        (uint32_t[]) {6, 3, 1},
     };
     uint32_t returned_strides[number_of_cases][MAX_RANK];
     uint32_t ranks[] = { 4, 2, 3, 1, 4, 3 };
@@ -209,23 +209,23 @@ START_TEST(test_reverse_permute)
 {
     uint32_t number_of_cases = 6;
 
-    uint32_t axis[][MAX_RANK] = {
-        {0},
-        {1, 0},
-        {2, 1, 0},
-        {1, 2, 0, 3},
-        {2, 0, 1},
-        {0, 1, 3, 2},
+    uint32_t *axis[] = {
+        (uint32_t[]) {0},
+        (uint32_t[]) {1, 0},
+        (uint32_t[]) {2, 1, 0},
+        (uint32_t[]) {1, 2, 0, 3},
+        (uint32_t[]) {2, 0, 1},
+        (uint32_t[]) {0, 1, 3, 2},
     };
 
     uint32_t returned_axis[number_of_cases][MAX_RANK];
-    uint32_t expected_axis[][MAX_RANK] = {
-        {0},
-        {1, 0},
-        {2, 1, 0},
-        {2, 0, 1, 3},
-        {1, 2, 0},
-        {0, 1, 3, 2},
+    uint32_t *expected_axis[] = {
+        (uint32_t[]) {0},
+        (uint32_t[]) {1, 0},
+        (uint32_t[]) {2, 1, 0},
+        (uint32_t[]) {2, 0, 1, 3},
+        (uint32_t[]) {1, 2, 0},
+        (uint32_t[]) {0, 1, 3, 2},
     };
     uint32_t ranks[] = { 1, 2, 3, 4, 3, 4 };
 
@@ -279,49 +279,49 @@ START_TEST(test_permute)
 {
     uint32_t number_of_cases = 6;
 
-    uint32_t original_shapes[][MAX_RANK] = {
-        {1},
-        {5, 3},
-        {3, 2, 1},
-        {2, 4, 3, 1},
-        {2, 2, 2},
-        {1, 1, 1, 1},
+    uint32_t *original_shapes[] = {
+        (uint32_t[]) {1},
+        (uint32_t[]) {5, 3},
+        (uint32_t[]) {3, 2, 1},
+        (uint32_t[]) {2, 4, 3, 1},
+        (uint32_t[]) {2, 2, 2},
+        (uint32_t[]) {1, 1, 1, 1},
     };
 
-    uint32_t original_strides[][MAX_RANK] = {
-        {0},
-        {3, 1},
-        {2, 1, 1},
-        {12, 3, 1, 1},
-        {4, 2, 1},
-        {0, 0, 0, 0},
+    uint32_t *original_strides[] = {
+        (uint32_t[]) {0},
+        (uint32_t[]) {3, 1},
+        (uint32_t[]) {2, 1, 1},
+        (uint32_t[]) {12, 3, 1, 1},
+        (uint32_t[]) {4, 2, 1},
+        (uint32_t[]) {0, 0, 0, 0},
     };
 
-    uint32_t axis[][MAX_RANK] = {
-        {0},
-        {1, 0},
-        {2, 1, 0},
-        {1, 2, 0, 3},
-        {2, 0, 1},
-        {0, 1, 3, 2},
+    uint32_t *axis[] = {
+        (uint32_t[]) {0},
+        (uint32_t[]) {1, 0},
+        (uint32_t[]) {2, 1, 0},
+        (uint32_t[]) {1, 2, 0, 3},
+        (uint32_t[]) {2, 0, 1},
+        (uint32_t[]) {0, 1, 3, 2},
     };
 
-    uint32_t expected_shapes[][MAX_RANK] = {
-        {1},
-        {3, 5},
-        {1, 2, 3},
-        {4, 3, 2, 1},
-        {2, 2, 2},
-        {1, 1, 1, 1},
+    uint32_t *expected_shapes[] = {
+        (uint32_t[]) {1},
+        (uint32_t[]) {3, 5},
+        (uint32_t[]) {1, 2, 3},
+        (uint32_t[]) {4, 3, 2, 1},
+        (uint32_t[]) {2, 2, 2},
+        (uint32_t[]) {1, 1, 1, 1},
     };
 
-    uint32_t expected_strides[][MAX_RANK] = {
-        {0},
-        {1, 3},
-        {1, 1, 2},
-        {3, 1, 12, 1},
-        {1, 4, 2},
-        {0, 0, 0, 0},
+    uint32_t *expected_strides[] = {
+        (uint32_t[]) {0},
+        (uint32_t[]) {1, 3},
+        (uint32_t[]) {1, 1, 2},
+        (uint32_t[]) {3, 1, 12, 1},
+        (uint32_t[]) {1, 4, 2},
+        (uint32_t[]) {0, 0, 0, 0},
     };
 
     uint32_t ranks[] = { 1, 2, 3, 4, 3, 4 };
@@ -455,6 +455,205 @@ START_TEST(test_permute_error)
 }
 END_TEST
 
+START_TEST(test_reduce_recover_dimension)
+{
+    uint32_t number_of_cases = 2;
+
+    uint32_t *original_shapes[] = {
+        (uint32_t[]) {2},
+        (uint32_t[]) {1, 2, 3},
+    };
+
+    uint32_t original_ranks[] = {
+        1,
+        3,
+    };
+
+    uint32_t *original_strides[] = {
+        (uint32_t[]) {1},
+        (uint32_t[]) {0, 3, 1},
+    };
+
+    uint32_t *expected_reduced_shapes[] = {
+        (uint32_t[]) {2, 1},
+        (uint32_t[]) {1, 2, 1, 1, 3},
+    };
+
+    uint32_t reduced_ranks[] = {
+        2,
+        5.
+    };
+
+    uint32_t *expected_reduced_strides[] = {
+        (uint32_t[]) {1, 0},
+        (uint32_t[]) {0, 3, 0, 0, 1},
+    };
+
+    uint32_t *axis[] = {
+        (uint32_t[]) {1},
+        (uint32_t[]) {2, 3},
+    };
+
+    uint32_t lengths[] = {
+        1,
+        2,
+    };
+    
+    uint32_t returned_reduced_shapes[number_of_cases][MAX_RANK];
+    uint32_t returned_reduced_strides[number_of_cases][MAX_RANK];
+
+    for (uint32_t i = 0; i < number_of_cases; i++)
+    {
+        error = reduce_recover_dimensions(original_shapes[i],
+                                          original_ranks[i],
+                                          original_strides[i],
+                                          returned_reduced_shapes[i],
+                                          reduced_ranks[i],
+                                          returned_reduced_strides[i],
+                                          axis[i],
+                                          lengths[i]);
+        ck_assert_ptr_null(error);
+        for (uint32_t j = 0; j < reduced_ranks[i]; j++)
+        {
+            ck_assert_uint_eq(returned_reduced_shapes[i][j], expected_reduced_shapes[i][j]);
+            ck_assert_uint_eq(returned_reduced_strides[i][j], expected_reduced_strides[i][j]);
+        }
+        error_destroy(error);
+        error = NULL;
+    }
+}
+END_TEST
+
+START_TEST(test_reduce_recover_dimension_error)
+{
+    uint32_t number_of_cases = 9;
+
+    uint32_t *original_shapes[] = {
+        NULL,
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+    };
+
+    uint32_t original_ranks[] = {
+        1,
+        1,
+        1,
+        1,
+        1,
+        0,
+        MAX_RANK + 1,
+        2,
+        1,
+    };
+
+    uint32_t *original_strides[] = {
+        (uint32_t[]) {1},
+        NULL,
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+    };
+
+    uint32_t *reduced_shapes[] = {
+        (uint32_t[]) {2, 1},
+        (uint32_t[]) {2, 1},
+        NULL,
+        (uint32_t[]) {2, 1},
+        (uint32_t[]) {2, 1},
+        (uint32_t[]) {2, 1},
+        (uint32_t[]) {2, 1},
+        (uint32_t[]) {2, 1},
+        (uint32_t[]) {2, 1},
+    };
+
+    uint32_t reduced_ranks[] = {
+        2,
+        2,
+        2,
+        2,
+        2,
+        0,
+        MAX_RANK + 1,
+        2,
+        2,
+    };
+
+    uint32_t *reduced_strides[] = {
+        (uint32_t[]) {2, 0},
+        (uint32_t[]) {2, 0},
+        (uint32_t[]) {2, 0},
+        NULL,
+        (uint32_t[]) {2, 0},
+        (uint32_t[]) {2, 0},
+        (uint32_t[]) {2, 0},
+        (uint32_t[]) {2, 0},
+        (uint32_t[]) {2, 0},
+    };
+
+    uint32_t *axis[] = {
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        NULL,
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {1},
+        (uint32_t[]) {2},
+    };
+
+    uint32_t lengths[] = {
+        1,
+        1,
+        1,
+        1,
+        1,
+        0,
+        MAX_RANK + 1,
+        1,
+        1,
+    };
+
+    error_type_t error_types[] = {
+        ERROR_NULL,
+        ERROR_NULL,
+        ERROR_NULL,
+        ERROR_NULL,
+        ERROR_NULL,
+        ERROR_RANK_CONFLICT,
+        ERROR_RANK_CONFLICT,
+        ERROR_RANK_CONFLICT,
+        ERROR_RANK_CONFLICT,
+    };
+
+    for (uint32_t i = 0; i < number_of_cases; i++)
+    {
+        error = reduce_recover_dimensions(original_shapes[i],
+                                          original_ranks[i],
+                                          original_strides[i],
+                                          reduced_shapes[i],
+                                          reduced_ranks[i],
+                                          reduced_strides[i],
+                                          axis[i],
+                                          lengths[i]);
+        ck_assert_ptr_nonnull(error);
+        ck_assert_int_eq(error->error_type, error_types[i]);
+        error_destroy(error);
+        error = NULL;
+    }
+}
+END_TEST
+
 Suite *make_view_suite(void)
 {
     Suite *s;
@@ -473,7 +672,8 @@ Suite *make_view_suite(void)
     tcase_add_test(tc, test_is_contiguous);
     tcase_add_test(tc, test_strides_from_shape);
     tcase_add_test(tc, test_strides_from_shape_error);
-
+    tcase_add_test(tc, test_reduce_recover_dimension);
+    tcase_add_test(tc, test_reduce_recover_dimension_error);
     suite_add_tcase(s, tc);
 
     return s;
