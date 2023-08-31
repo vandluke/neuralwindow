ee7836736ef2b952d56e9cee2440a29ff80241f6
vandluke <lvand10011@gmail.com>
2023-08-31 07:01:35 -0400

    exponential unit tests with torch

--- test/test_buffer.cc
+++ test/test_buffer.cc
@@ -8,18 +8,37 @@ extern "C"
 
 #define CASES 2
 #define EPSILON 0.0001
+#define SEED 1234
+
+bool_t set_seed = true;
 
 nw_error_t *unary_error;
+
 buffer_t *unary_buffers[CASES];
+buffer_t *returned_unary_buffers[CASES];
+buffer_t *expected_unary_buffers[CASES];
+
+view_t *views[CASES];
+view_t *returned_views[CASES];
+view_t *expected_views[CASES];
 
 void unary_setup(void)
 {
-    // view_t *views[CASES];
+    if (set_seed)
+    {
+        torch::manual_seed(SEED);
+        set_seed = false;
+    }
 
     for (int i = 0; i < CASES; i++)
     {
         unary_buffers[i] = NULL;
-        // views[i] = NULL;
+        returned_unary_buffers[i] = NULL;
+        expected_unary_buffers[i] = NULL;
+
+        views[i] = NULL;
+        returned_views[i] = NULL;
+        expected_views[i] = NULL;
     }
 }
 
@@ -28,18 +47,45 @@ void unary_teardown(void)
     for (int i = 0; i < CASES; i++)
     {
         buffer_destroy(unary_buffers[i]);
+        buffer_destroy(returned_unary_buffers[i]);
+        buffer_destroy(expected_unary_buffers[i]);
     }
     error_destroy(unary_error);
 }
 
-// START_TEST(test_exponential)
-// {
-//     view_t *expected_view;
-//     view_t *returned_view;
-//     buffer_t *expected_unary_buffer;
-//     buffer_t *returned_unary_buffer;
-// }
-// END_TEST
+START_TEST(test_exponential)
+{
+    for (int i = 0; i < CASES; ++i)
+    {
+        torch::Tensor tensor = torch::randn({3, 3}, torch::TensorOptions().dtype(torch::kFloat32));
+        torch::Tensor expected_tensor = torch::exp(tensor);
+
+        unary_error = view_create(&views[i], (uint32_t) tensor.storage_offset(), (uint32_t) tensor.ndimension(), (uint32_t *) tensor.sizes().data(), NULL);
+        ck_assert_ptr_null(unary_error);
+        unary_error = buffer_create(&unary_buffers[i], OPENBLAS_RUNTIME, FLOAT32, views[i], (void *) tensor.data_ptr(), (uint32_t) tensor.numel(), true);
+        ck_assert_ptr_null(unary_error);
+
+        unary_error = view_create(&returned_views[i], (uint32_t) tensor.storage_offset(), (uint32_t) tensor.ndimension(), (uint32_t *) tensor.sizes().data(), NULL);
+        ck_assert_ptr_null(unary_error);
+        unary_error = buffer_create(&returned_unary_buffers[i], OPENBLAS_RUNTIME, FLOAT32, returned_views[i], NULL, (uint32_t) tensor.numel(), true);
+        ck_assert_ptr_null(unary_error);
+
+        unary_error = view_create(&expected_views[i], (uint32_t) expected_tensor.storage_offset(), (uint32_t) expected_tensor.ndimension(), (uint32_t *) expected_tensor.sizes().data(), NULL);
+        ck_assert_ptr_null(unary_error);
+        unary_error = buffer_create(&expected_unary_buffers[i], OPENBLAS_RUNTIME, FLOAT32, expected_views[i], (void *) expected_tensor.data_ptr(), (uint32_t) expected_tensor.numel(), true);
+        ck_assert_ptr_null(unary_error);
+
+        unary_error = runtime_exponential(unary_buffers[i], returned_unary_buffers[i]);
+        ck_assert_ptr_null(unary_error);
+
+        for (int j = 0; j < expected_tensor.numel(); ++j)
+        {
+            ck_assert_double_eq_tol(((float32_t *) returned_unary_buffers[i]->data)[j],
+                                    ((float32_t *) expected_unary_buffers[i]->data)[j], EPSILON);
+        }
+    }
+}
+END_TEST
 
 Suite *make_buffer_suite(void)
 {
@@ -49,7 +95,7 @@ Suite *make_buffer_suite(void)
     s = suite_create("Test Unary Suite");
     tc_unary = tcase_create("Unary Case");
     tcase_add_checked_fixture(tc_unary, unary_setup, unary_teardown);
-    // tcase_add_test(tc_unary, test_exponential);
+    tcase_add_test(tc_unary, test_exponential);
     suite_add_tcase(s, tc_unary);
 
     return s;
