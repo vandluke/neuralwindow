57bf370dac663dfd466da0fbc203a8a1cca705ae
vandluke <lvand10011@gmail.com>
2023-09-01 06:09:27 -0400

    refactor unary unit tests and implement exponential unit tets

--- test/test_buffer.cc
+++ test/test_buffer.cc
@@ -6,15 +6,13 @@ extern "C"
 }
 #include <torch/torch.h>
 
-#define CASES 2
+#define CASES 6
 #define EPSILON 0.0001
 #define SEED 1234
 
 bool_t set_seed = true;
 
 nw_error_t *unary_error;
-
-
 
 buffer_t *unary_buffers[CASES];
 buffer_t *returned_unary_buffers[CASES];
@@ -23,6 +21,8 @@ buffer_t *expected_unary_buffers[CASES];
 view_t *views[CASES];
 view_t *returned_views[CASES];
 view_t *expected_views[CASES];
+
+torch::Tensor tensors[CASES];
 
 void unary_setup(void)
 {
@@ -42,6 +42,77 @@ void unary_setup(void)
         returned_views[i] = NULL;
         expected_views[i] = NULL;
     }
+
+    std::vector<int64_t> shapes[] = {
+        {1},
+        {1},
+        {1},
+        {1},
+        {3, 4, 5},
+        {3, 4, 5},
+    };
+    
+    runtime_t runtimes[] = {
+        OPENBLAS_RUNTIME,
+        MKL_RUNTIME,
+        OPENBLAS_RUNTIME,
+        MKL_RUNTIME,
+        OPENBLAS_RUNTIME,
+        MKL_RUNTIME,
+    };
+
+    datatype_t datatypes[] = {
+        FLOAT32,
+        FLOAT32,
+        FLOAT64,
+        FLOAT64,
+        FLOAT32,
+        FLOAT32,
+    };
+
+    torch::ScalarType torch_datatypes[] = {
+        torch::kFloat32,
+        torch::kFloat32,
+        torch::kFloat64,
+        torch::kFloat64,
+        torch::kFloat32,
+        torch::kFloat32,
+    };
+
+    for (int i = 0; i < CASES; ++i)
+    {
+        tensors[i] = torch::randn(shapes[i], torch::TensorOptions().dtype(torch_datatypes[i]));
+
+        unary_error = view_create(&views[i], 
+                                  (uint64_t) tensors[i].storage_offset(),
+                                  (uint64_t) tensors[i].ndimension(),
+                                  (uint64_t *) tensors[i].sizes().data(),
+                                  NULL);
+        ck_assert_ptr_null(unary_error);
+        unary_error = buffer_create(&unary_buffers[i],
+                                    runtimes[i],
+                                    datatypes[i],
+                                    views[i],
+                                    (void *) tensors[i].data_ptr(),
+                                    (uint64_t) tensors[i].numel(),
+                                    true);
+        ck_assert_ptr_null(unary_error);
+
+        unary_error = view_create(&returned_views[i],
+                                  (uint64_t) tensors[i].storage_offset(),
+                                  (uint64_t) tensors[i].ndimension(),
+                                  (uint64_t *) tensors[i].sizes().data(),
+                                  NULL);
+        ck_assert_ptr_null(unary_error);
+        unary_error = buffer_create(&returned_unary_buffers[i],
+                                    runtimes[i],
+                                    datatypes[i],
+                                    returned_views[i],
+                                    NULL,
+                                    (uint64_t) tensors[i].numel(),
+                                    true);
+        ck_assert_ptr_null(unary_error);
+    }
 }
 
 void unary_teardown(void)
@@ -55,79 +126,63 @@ void unary_teardown(void)
     error_destroy(unary_error);
 }
 
+void ck_assert_buffer_eq(buffer_t *returned_buffer, buffer_t *expected_buffer)
+{
+    ck_assert_uint_eq(expected_buffer->view->rank, returned_buffer->view->rank);
+    ck_assert_uint_eq(expected_buffer->view->offset, returned_buffer->view->offset);
+    ck_assert_uint_eq(expected_buffer->n, returned_buffer->n);
+    ck_assert_uint_eq(expected_buffer->size, returned_buffer->size);
+    ck_assert_int_eq(expected_buffer->datatype, returned_buffer->datatype);
+    ck_assert_int_eq(expected_buffer->runtime, returned_buffer->runtime);
+
+    for (uint64_t i = 0; i < expected_buffer->view->rank; ++i)
+    {
+        ck_assert_uint_eq(expected_buffer->view->shape[i], returned_buffer->view->shape[i]);
+        ck_assert_uint_eq(expected_buffer->view->strides[i], returned_buffer->view->strides[i]);
+    }
+
+    for (uint64_t i = 0; i < expected_buffer->n; ++i)
+    {
+        switch (expected_buffer->datatype)
+        {
+        case FLOAT32:
+            ck_assert_float_eq_tol(((float32_t *) returned_buffer->data)[i],
+                                    ((float32_t *) expected_buffer->data)[i], EPSILON);
+            break;
+        case FLOAT64:
+            ck_assert_double_eq_tol(((float64_t *) returned_buffer->data)[i],
+                                    ((float64_t *) expected_buffer->data)[i], EPSILON);
+        default:
+            break;
+        }
+    }
+}
+
 START_TEST(test_exponential)
 {
-    std::vector<int64_t> shapes[] = {
-        {3, 4, 5},
-        {3, 4, 5},
-    };
-    
-    runtime_t runtimes[] = {
-        OPENBLAS_RUNTIME,
-        MKL_RUNTIME,
-    };
-
-    datatype_t datatypes[] = {
-        FLOAT32,
-        FLOAT32,
-    };
-
-    torch::ScalarType torch_datatypes[] = {
-        torch::kFloat32,
-        torch::kFloat32,
-    };
-
     for (int i = 0; i < CASES; ++i)
     {
-        torch::Tensor tensor = torch::randn(shapes[i], torch::TensorOptions().dtype(torch_datatypes[i]));
-        torch::Tensor expected_tensor = torch::exp(tensor);
+        torch::Tensor expected_tensor = torch::exp(tensors[i]);
 
-        unary_error = view_create(&views[i], (uint64_t) tensor.storage_offset(), (uint64_t) tensor.ndimension(), (uint64_t *) tensor.sizes().data(), NULL);
+        unary_error = view_create(&expected_views[i],
+                                  (uint64_t) expected_tensor.storage_offset(),
+                                  (uint64_t) expected_tensor.ndimension(),
+                                  (uint64_t *) expected_tensor.sizes().data(),
+                                  NULL);
         ck_assert_ptr_null(unary_error);
-        unary_error = buffer_create(&unary_buffers[i], runtimes[i], datatypes[i], views[i], (void *) tensor.data_ptr(), (uint64_t) tensor.numel(), true);
-        ck_assert_ptr_null(unary_error);
-
-        unary_error = view_create(&returned_views[i], (uint64_t) tensor.storage_offset(), (uint64_t) tensor.ndimension(), (uint64_t *) tensor.sizes().data(), NULL);
-        ck_assert_ptr_null(unary_error);
-        unary_error = buffer_create(&returned_unary_buffers[i], runtimes[i], datatypes[i], returned_views[i], NULL, (uint64_t) tensor.numel(), true);
-        ck_assert_ptr_null(unary_error);
-
-        unary_error = view_create(&expected_views[i], (uint64_t) expected_tensor.storage_offset(), (uint64_t) expected_tensor.ndimension(), (uint64_t *) expected_tensor.sizes().data(), NULL);
-        ck_assert_ptr_null(unary_error);
-        unary_error = buffer_create(&expected_unary_buffers[i], runtimes[i], datatypes[i], expected_views[i], (void *) expected_tensor.data_ptr(), (uint64_t) expected_tensor.numel(), true);
+        unary_error = buffer_create(&expected_unary_buffers[i],
+                                    unary_buffers[i]->runtime,
+                                    unary_buffers[i]->datatype,
+                                    expected_views[i],
+                                    (void *) expected_tensor.data_ptr(),
+                                    (uint64_t) expected_tensor.numel(),
+                                    true);
         ck_assert_ptr_null(unary_error);
 
         unary_error = runtime_exponential(unary_buffers[i], returned_unary_buffers[i]);
         ck_assert_ptr_null(unary_error);
 
-        ck_assert_uint_eq(expected_views[i]->rank, returned_views[i]->rank);
-        ck_assert_uint_eq(expected_views[i]->offset, returned_views[i]->offset);
-        ck_assert_uint_eq(expected_unary_buffers[i]->n, returned_unary_buffers[i]->n);
-        ck_assert_uint_eq(expected_unary_buffers[i]->size, returned_unary_buffers[i]->size);
-        ck_assert_int_eq(expected_unary_buffers[i]->datatype, returned_unary_buffers[i]->datatype);
-        ck_assert_int_eq(expected_unary_buffers[i]->runtime, returned_unary_buffers[i]->runtime);
-
-        for (uint64_t j = 0; j < expected_views[i]->rank; ++j)
-        {
-            ck_assert_uint_eq(expected_views[i]->shape[j], returned_views[i]->shape[j]);
-            ck_assert_uint_eq(expected_views[i]->strides[j], returned_views[i]->strides[j]);
-        }
-
-        for (int j = 0; j < expected_tensor.numel(); ++j)
-        {
-            switch (datatypes[i])
-            {
-            case FLOAT32:
-                ck_assert_float_eq_tol(((float32_t *) returned_unary_buffers[i]->data)[j],
-                                       ((float32_t *) expected_unary_buffers[i]->data)[j], EPSILON);
-                break;
-            case FLOAT64:
-                ck_assert_double_eq_tol(((float64_t *) returned_unary_buffers[i]->data)[j],
-                                        ((float64_t *) expected_unary_buffers[i]->data)[j], EPSILON);
-            default:
-                break;
-            }
-        }
+        ck_assert_buffer_eq(returned_unary_buffers[i], expected_unary_buffers[i]);
     }
 }
 END_TEST
