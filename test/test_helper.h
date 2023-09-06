2534b74c06bba8d336cc69c718d0a4f903c832c0
vandluke <lvand10011@gmail.com>
2023-09-06 16:07:51 -0400

    reduction unit tests still failing

--- test/test_helper.h
+++ test/test_helper.h
@@ -6,8 +6,8 @@ typedef struct function_t function_t;
 typedef struct buffer_t buffer_t;
 typedef struct view_t view_t;
 
-#define EPSILON 0.0001
-#define SEED 1234
+#define EPSILON 0.001
+#define SEED 123
 
 void ck_assert_view_eq(const view_t *returned_view, const view_t *expected_view);
 void ck_assert_buffer_eq(const buffer_t *returned_buffer, const buffer_t *expected_buffer);
