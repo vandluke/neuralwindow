941e9e9484550fa4ac783840c7ddcc3126760b3a
vandenbergheluke <vandenbergheluke@gmail.com>
2023-09-22 08:14:28 -0400

    binary unit tests

--- src/runtime/openblas_runtime.c
+++ src/runtime/openblas_runtime.c
@@ -7,7 +7,7 @@
 #include <cblas.h>
 #include <math.h>
 
-#define EPSILON 0.0000001
+#define EPSILON 1e-7
 
 nw_error_t *openblas_memory_allocate(void **pp, size_t size)
 {
@@ -452,17 +452,23 @@ void openblas_power(datatype_t datatype, uint64_t n, const void *x_data, uint64_
 
 static void openblas_compare_equal_float32(int n, const float32_t *x_data, int x_stride, const float32_t *y_data, int y_stride, float32_t *z_data, int z_stride)
 {
+    float32_t x, y;
     for (int i = 0; i < n; ++i)
     {
-        z_data[i * z_stride] = fabsf(x_data[i * x_stride] - y_data[i * y_stride]) < EPSILON ? (float32_t) 1.0 : (float32_t) 0.0;
+        x = x_data[i * x_stride];
+        y = y_data[i * y_stride];
+        z_data[i * z_stride] = fabsf(x - y) < EPSILON * (fabsf(x) + fabsf(y)) ? (float32_t) 1.0 : (float32_t) 0.0;
     }
 }
 
 static void openblas_compare_equal_float64(int n, const float64_t *x_data, int x_stride, const float64_t *y_data, int y_stride, float64_t *z_data, int z_stride)
 {
+    float64_t x, y;
     for (int i = 0; i < n; ++i)
     {
-        z_data[i * z_stride] = fabs(x_data[i * x_stride] - y_data[i * y_stride]) < EPSILON ? (float64_t) 1.0 : (float64_t) 0.0;
+        x = x_data[i * x_stride];
+        y = y_data[i * y_stride];
+        z_data[i * z_stride] = fabs(x - y) < EPSILON * (fabs(x) + fabs(y)) ? (float64_t) 1.0 : (float64_t) 0.0;
     }
 }
 
