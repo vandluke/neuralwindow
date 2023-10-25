437949f6a7a8e9ea0a7710a8092d273b94e6bce6
Ethan Leir <eleir9267@gmail.com>
2023-10-25 14:31:52 -0400

    Fix for 0 block size bug.

--- src/runtime/cu_runtime.cu
+++ src/runtime/cu_runtime.cu
@@ -98,6 +98,10 @@ extern "C" void cu_exponential(datatype_t datatype, int64_t n, const void *x_dat
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_exponential_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_exponential_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride);
@@ -114,6 +118,10 @@ extern "C" void cu_exponential(datatype_t datatype, int64_t n, const void *x_dat
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_exponential_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_exponential_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride);
@@ -159,6 +167,10 @@ extern "C" void cu_logarithm(datatype_t datatype, int64_t n, const void *x_data,
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_logarithm_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_logarithm_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride);
@@ -173,6 +185,10 @@ extern "C" void cu_logarithm(datatype_t datatype, int64_t n, const void *x_data,
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_logarithm_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_logarithm_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride);
@@ -218,6 +234,10 @@ extern "C" void cu_sine(datatype_t datatype, int64_t n, const void *x_data, int6
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_sine_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_sine_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride);
@@ -232,6 +252,10 @@ extern "C" void cu_sine(datatype_t datatype, int64_t n, const void *x_data, int6
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_sine_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_sine_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride);
@@ -277,6 +301,10 @@ extern "C" void cu_cosine(datatype_t datatype, int64_t n, const void *x_data, in
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_cosine_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_cosine_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride);
@@ -291,6 +319,10 @@ extern "C" void cu_cosine(datatype_t datatype, int64_t n, const void *x_data, in
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_cosine_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         cu_cosine_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride);
 
 #ifdef DEBUG
@@ -334,6 +366,10 @@ extern "C" void cu_square_root(datatype_t datatype, int64_t n, const void *x_dat
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_square_root_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_square_root_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride);
@@ -348,6 +384,10 @@ extern "C" void cu_square_root(datatype_t datatype, int64_t n, const void *x_dat
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_square_root_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_square_root_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride);
@@ -393,6 +433,10 @@ extern "C" void cu_reciprocal(datatype_t datatype, int64_t n, const void *x_data
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_reciprocal_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_reciprocal_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride);
@@ -407,6 +451,10 @@ extern "C" void cu_reciprocal(datatype_t datatype, int64_t n, const void *x_data
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_reciprocal_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_reciprocal_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride);
@@ -476,6 +524,10 @@ void cu_negation(datatype_t datatype,
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_negation_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_negation_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride);
@@ -490,6 +542,10 @@ void cu_negation(datatype_t datatype,
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_negation_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_negation_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride);
@@ -537,6 +593,10 @@ extern "C" void cu_rectified_linear(datatype_t datatype, int64_t n, const void *
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_rectified_linear_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_rectified_linear_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride);
@@ -551,6 +611,10 @@ extern "C" void cu_rectified_linear(datatype_t datatype, int64_t n, const void *
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_rectified_linear_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_rectified_linear_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride);
@@ -612,6 +676,10 @@ extern "C" void cu_sigmoid(datatype_t datatype, int64_t n, const void *x_data, i
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_sigmoid_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_sigmoid_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride);
@@ -626,6 +694,10 @@ extern "C" void cu_sigmoid(datatype_t datatype, int64_t n, const void *x_data, i
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_sigmoid_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_sigmoid_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride);
@@ -842,6 +914,10 @@ extern "C" void cu_multiplication(datatype_t datatype,
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_multiplication_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_multiplication_float32<<<grid_size, block_size>>>((int) n,
@@ -861,6 +937,12 @@ extern "C" void cu_multiplication(datatype_t datatype,
     case FLOAT64:
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_multiplication_float64, 0, 0);
+
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
+        grid_size = (n + block_size - 1) / block_size;
 
         cu_multiplication_float64<<<grid_size, block_size>>>((int) n,
                                   &((float64_t *) x_data)[x_offset],
@@ -911,6 +993,10 @@ extern "C" void cu_division(datatype_t datatype, int64_t n, const void *x_data, 
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_division_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_division_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride, &((float32_t *) z_data)[z_offset], (int) z_stride);
@@ -925,6 +1011,10 @@ extern "C" void cu_division(datatype_t datatype, int64_t n, const void *x_data, 
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_division_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_division_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride, &((float64_t *) z_data)[z_offset], (int) z_stride);
@@ -970,6 +1060,10 @@ extern "C" void cu_power(datatype_t datatype, int64_t n, const void *x_data, int
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_power_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_power_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride, &((float32_t *) z_data)[z_offset], (int) z_stride);
@@ -984,6 +1078,10 @@ extern "C" void cu_power(datatype_t datatype, int64_t n, const void *x_data, int
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_power_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_power_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride, &((float64_t *) z_data)[z_offset], (int) z_stride);
@@ -1035,6 +1133,10 @@ extern "C" void cu_compare_equal(datatype_t datatype, int64_t n, const void *x_d
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_compare_equal_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_compare_equal_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride, &((float32_t *) z_data)[z_offset], (int) z_stride);
@@ -1049,6 +1151,10 @@ extern "C" void cu_compare_equal(datatype_t datatype, int64_t n, const void *x_d
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_compare_equal_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_compare_equal_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride, &((float64_t *) z_data)[z_offset], (int) z_stride);
@@ -1094,6 +1200,10 @@ extern "C" void cu_compare_greater(datatype_t datatype, int64_t n, const void *x
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_compare_greater_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_compare_greater_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset], (int) y_stride, &((float32_t *) z_data)[z_offset], (int) z_stride);
@@ -1108,6 +1218,10 @@ extern "C" void cu_compare_greater(datatype_t datatype, int64_t n, const void *x
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_compare_greater_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_compare_greater_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset], (int) y_stride, &((float64_t *) z_data)[z_offset], (int) z_stride);
@@ -1296,6 +1410,10 @@ extern "C" void cu_maximum(datatype_t datatype, int64_t n, const void *x_data, i
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_maximum_float32, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_maximum_float32<<<grid_size, block_size>>>((int) n, &((float32_t *) x_data)[x_offset], (int) x_stride, &((float32_t *) y_data)[y_offset]);
@@ -1310,6 +1428,10 @@ extern "C" void cu_maximum(datatype_t datatype, int64_t n, const void *x_data, i
         cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
                 cu_maximum_float64, 0, 0);
 
+        if (block_size == 0)
+        {
+            block_size = 32;
+        }
         grid_size = (n + block_size - 1) / block_size;
 
         cu_maximum_float64<<<grid_size, block_size>>>((int) n, &((float64_t *) x_data)[x_offset], (int) x_stride, &((float64_t *) y_data)[y_offset]);
