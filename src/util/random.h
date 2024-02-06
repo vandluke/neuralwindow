426c379db5135f22abfe8276a77f3dd989b2861f
vandenbergheluke <vandenbergheluke@gmail.com>
2024-02-06 09:19:13 -0500

    multinomial implementation c

--- src/util/random.h
+++ src/util/random.h
@@ -15,5 +15,7 @@ float64_t uniform(float64_t lower_bound, float64_t upper_bound);
 float32_t normalf(float32_t mean, float32_t standard_deviation);
 float64_t normal(float64_t mean, float64_t standard_deviation);
 void shuffle_array(int64_t *array, int64_t length);
+float32_t multinomialf(float32_t *probabilities, int64_t length);
+float64_t multinomial(float64_t *probabilities, int64_t length);
 
 #endif
