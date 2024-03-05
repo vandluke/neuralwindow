14df26b0964b72554a7811db0f5d10eee725dbd7
vandenbergheluke <vandenbergheluke@gmail.com>
2024-03-05 13:38:30 -0500

    random seed and demo instructions

--- example/transformer.c
+++ example/transformer.c
@@ -12,6 +12,7 @@
 #include <measure.h>
 #include <random.h>
 #include <string.h>
+#include <time.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -772,6 +773,8 @@ int main(int argc, char **argv)
 
     if (demo_var && strcmp(demo_var, "1") == 0)
     {
+        set_seed(time(NULL));
+
         if (argc < 2)
         {
             error = ERROR(ERROR_ARGUMENTS, string_create("invalid number of arguments."), error);
